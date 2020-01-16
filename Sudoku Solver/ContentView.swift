b348aaf56daf9666c96d186e18da8fc20e541a32
Taylor <taylorweil98@gmail.com>
2020-01-16 14:12:50 -0600

    Initial Commit

---
+++ Sudoku Solver/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sudoku Solver
+//
+//  Created by Taylor Weil on 1/16/20.
+//  Copyright © 2020 Taylor Weil. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
